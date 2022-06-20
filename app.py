ceb9c9bddd2201e01362d919c08494a1b09cd270
eyoo95 <105832377+eyoo95@users.noreply.github.com>
2022-06-20 10:16:23 +0900

    add

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from http import HTTPStatus
 from flask_restful import Api
 from resources.recipe import RecipeListResource
 from resources.recipe_info import RecipeResource
+from resources.recipe_publish import RecipePublishResource
 
 app = Flask(__name__)
 
@@ -11,6 +12,7 @@ api = Api(app)
 # 경로와 resource(API 코드)를 연결한다.
 api.add_resource(RecipeListResource , '/recipes')
 api.add_resource(RecipeResource , '/recipes/<int:recipe_id>')
+api.add_resource(RecipePublishResource, '/recipes/<int:recipe_id>/publish')
 
 if __name__ == '__main__' :
     app.run()
