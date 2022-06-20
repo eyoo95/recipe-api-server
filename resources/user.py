ca6ef74f9d1e62a0300f858c58e59e614bc74ac5
eyoo95 <105832377+eyoo95@users.noreply.github.com>
2022-06-20 11:35:21 +0900

    Update user.py

--- resources/user.py
+++ resources/user.py
@@ -4,6 +4,7 @@ from flask_restful import Resource
 from mysql.connector.errors import Error
 from mysql_connection import get_connection
 import mysql.connector
+from email-validator import validate_email, EmailNotValidError
 
 class UserRegisterResource(Resource):
     def post(self):
@@ -16,7 +17,20 @@ class UserRegisterResource(Resource):
         data = request.get_json()
 
         # 이메일 주소형식을 확인한다.
-
         
+
+        email = "my+address@mydomain.tld"
+
+        try:
+        # Validate & take the normalized form of the email
+        # address for all logic beyond this point (especially
+        # before going to a database query where equality
+        # does not take into account normalization).
+        email = validate_email(email).email
+        except EmailNotValidError as e:
+        # email is not valid, exception message is human-readable
+        print(str(e))
+
+
 
         return
